546a2b943054275932112bec43a6ac3cf3526e96
Pranav Shikarpur <contact@snpranav.com>
2020-09-12 18:37:54 -0400

    Updated Script

--- gtop-docker.sh
+++ gtop-docker.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+sudo tee /usr/local/bin/gtop > /dev/null << EOF
 docker run --rm -it \
 	--name gtop-from-executable \
 	--net="host" \
 	--pid="host" \
 	aksakalli/gtop
+EOF
+
+sudo chmod 100 /usr/local/bin/gtop
